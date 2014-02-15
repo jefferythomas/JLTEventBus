eb8d8c8daacd9650267a3dc8b9dda8052b9eea07
Jeffery Thomas <jeffery.thomas@gmail.com>
2014-02-15 16:47:44 -0500

    Added empty JLTEventBus class

---
+++ JLTEventBus.h
@@ -0,0 +1,13 @@
+//
+//  JLTEventBus.h
+//  JLTEventBusDemo
+//
+//  Created by Jeffery Thomas on 2/15/14.
+//  Copyright (c) 2014 JLT Source. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JLTEventBus : NSObject
+
+@end
---
+++ JLTEventBus.m
@@ -0,0 +1,13 @@
+//
+//  JLTEventBus.m
+//  JLTEventBusDemo
+//
+//  Created by Jeffery Thomas on 2/15/14.
+//  Copyright (c) 2014 JLT Source. All rights reserved.
+//
+
+#import "JLTEventBus.h"
+
+@implementation JLTEventBus
+
+@end
